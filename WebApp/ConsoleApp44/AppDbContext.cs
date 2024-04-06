d6a95d6306137c688b0d2c346dc75731c55bc4da
UMMANCODE <ummanmm@code.edu.az>
2024-04-06 15:30:44 +0400

    Backend Added

---
+++ WebApp/ConsoleApp44/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp44.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp44 {
+    public class AppDbContext : DbContext {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=DELL-UMMAN\\SQLEXPRESS;Database=WebApp1;Trusted_Connection=True;Trust Server Certificate=True");
+        }
+    }
+}
---
+++ WebApp/ConsoleApp44/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp44;
+using ConsoleApp44.Models;
+
+var appDbContext = new AppDbContext();
+
+var appBuilder = WebApplication.CreateBuilder();
+
+appBuilder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = appBuilder.Build();
+
+app.UseCors();
+
+app.MapGet("/", () => {
+    return "Hello, World!";
+});
+
+app.MapGet("/products", () => {
+    return appDbContext.Products.ToList();
+});
+
+app.MapGet("/products/{id}", (int id) => {
+    var data = appDbContext.Products.Find(id);
+
+    if (data == null) return Results.NotFound("product not found!");
+
+    return Results.Ok(data);
+});
+
+app.MapPost("/products", (Product product) => {
+    appDbContext.Add(product);
+    appDbContext.SaveChanges();
+
+    return Results.Ok();
+});
+
+
+app.Run("http://localhost:5000");
